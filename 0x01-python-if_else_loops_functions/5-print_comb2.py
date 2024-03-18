c24d4485fe12a92b6f72e9d19b75e61b11a4676d
Manyouu <pharaohmanyou00@yahoo.com>
2024-03-18 09:13:08 -0700

    all task

---
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+for num in range(100):
+    if num == 99:
+        print("{}".format(num))
+    elif num < 99:
+        if num < 10:
+            num = "0" + str(num)
+        print("{}".format(num), end=", ")
