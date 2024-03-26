2799a1d9a811795d39b43b74d918d87d9e35800e
Manyouu <pharaohmanyou00@yahoo.com>
2024-03-26 04:22:12 -0700

    all update

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
 def islower(c):
-    # Check if the character is within the lowercase ASCII range
-    return ord('a') <= ord(c) <= ord('z')
-
-# Example usage:
-print(islower('a'))  # True
-print(islower('A'))  # False
-print(islower('z'))  # True
-print(islower('Z'))  # False
+    if ord(c) > 96 and ord(c) < 123:
+        return True
+    return False
--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    # Get the last digit using modulus
-    last_digit = number % 10
-    print("The last digit of", number, "is", last_digit)
+    last_digit = abs(number) % 10
+    print('{:d}'.format(last_digit), end='')
+    return last_digit
 
 # Example usage:
-print_last_digit(123)  # Output: The last digit of 123 is 3
-print_last_digit(9876)  # Output: The last digit of 9876 is 6 
+number_to_check = -12345
+print_last_digit(number_to_check) 
