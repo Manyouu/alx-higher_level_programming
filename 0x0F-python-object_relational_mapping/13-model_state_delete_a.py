a966bf8978b0bc414b9777d5f7a96cb4a4272945
Manyouu <pharaohmanyou00@yahoo.com>
2024-06-28 12:35:45 -0700

    update

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,18 +1,31 @@
 #!/usr/bin/python3
-""" prints the State object with the name passed as argument from the database
-"""
-import sys
-from model_state import Base, State
-from sqlalchemy import (create_engine)
+'''script for task 13'''
+
+from model_state import State, Base
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+import sys
 
 
-if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
-    Base.metadata.create_all(engine)
+if __name__ == '__main__':
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    host = 'localhost'
+    port = '3306'
+
+    engine = create_engine('mysql+mysqldb://{}:{}@{}:{}/{}'.format(
+                           username, password, host, port, db_name),
+                           pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
-    session = Session()
-    for instance in session.query(State).filter(State.name.like('%a%')):
-        session.delete(instance)
-    session.commit()
+    local_session = Session()
+    states = local_session.query(State).filter(
+                           State.name.op('regexp')('.*a+.*')
+                           )
+
+    for state in states:
+        local_session.delete(state)
+    local_session.commit()
+
+    local_session.close()
+    engine.dispose()
